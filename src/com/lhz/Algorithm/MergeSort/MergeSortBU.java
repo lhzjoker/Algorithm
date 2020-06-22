f2f5491132cba4745a3de77db229bd1519dda773
lhzjoker <2313169248@qq.com>
2020-06-22 11:50:16 +0800

    归并排序

--- src/com/lhz/Algorithm/MergeSort/MergeSortBU.java
+++ src/com/lhz/Algorithm/MergeSort/MergeSortBU.java
@@ -27,27 +27,27 @@ public class MergeSortBU {
     public static void sort(Comparable[] arr) {
         int n = arr.length;
         //sz为元素数组的大小
-        for (int sz = 1; sz < n; sz *= 2) {
-            for (int i = 0; i + sz < n; i += sz + sz) {
-                // 对 arr[i...i+sz-1] 和 arr[i+sz...i+2*sz-1] 进行归并
-                merge(arr, i, i + sz - 1, Math.min(i + sz + sz - 1, n - 1));
-            }
-        }
+//        for (int sz = 1; sz < n; sz *= 2) {
+//            for (int i = 0; i + sz < n; i += sz + sz) {
+//                // 对 arr[i...i+sz-1] 和 arr[i+sz...i+2*sz-1] 进行归并
+//                merge(arr, i, i + sz - 1, Math.min(i + sz + sz - 1, n - 1));
+//            }
+//        }
 
         //改进
         //对小数组进行插入排序
-//        for (int i = 0; i < n; i += 16) {
-//            InsertionSort.sort(arr, i, Math.min(i + 15, n - 1));
-//        }
-//
-//        for (int sz = 16; sz < n; sz *= 2) {
-//            for (int i = 0; i + sz < n; i += sz + sz) {
-//                //只有当左边还有数大于右边时才排序，否则已经排好序了，能快蛮多
-//                if (arr[i + sz - 1].compareTo(arr[i + sz]) > 0) {
-//                    merge(arr, i, i + sz - 1, Math.min(i + 2 * sz - 1, n - 1));
-//                }
-//            }
-//        }
+        for (int i = 0; i < n; i += 16) {
+            InsertionSort.sort(arr, i, Math.min(i + 15, n - 1));
+        }
+
+        for (int sz = 16; sz < n; sz *= 2) {
+            for (int i = 0; i + sz < n; i += sz + sz) {
+                //只有当左边还有数大于右边时才排序，否则已经排好序了，能快蛮多
+                if (arr[i + sz - 1].compareTo(arr[i + sz]) > 0) {
+                    merge(arr, i, i + sz - 1, Math.min(i + 2 * sz - 1, n - 1));
+                }
+            }
+        }
     }
 
     public static void merge(Comparable[] arr, int l, int mid, int r) {
